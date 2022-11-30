95869eb4a8149aa33a39da138e44e9b2c13dbefb
jskomal <90876852+jskomal@users.noreply.github.com>
2022-11-29 17:15:37 -0700

    Add BPM to Song component

--- src/components/PianoRoll/PianoRoll.js
+++ src/components/PianoRoll/PianoRoll.js
@@ -3,57 +3,55 @@ import { Song, Track, Instrument } from "reactronica";
 import PianoRollDisplay from "../PianoRollDisplay/PianoRollDisplay.js";
 import "./PianoRoll.css";
 
-const PianoRoll = ({ kit }) => {
-  const [isPlaying, setIsPlaying] = useState(false);
+const PianoRoll = ({ kit, bpm }) => {
+	const [isPlaying, setIsPlaying] = useState(false);
 
-  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+	const [currentStepIndex, setCurrentStepIndex] = useState(0);
 
-  const [steps, setSteps] = useState(kit.sequence);
-  return (
-    <>
-      {kit && (
-        <section className="piano-roll fade-in">
-          <button
-            className="play-button"
-            onClick={() => setIsPlaying(!isPlaying)}
-          >
-            {isPlaying ? " STOP " : " PLAY "}
-          </button>
+	const [steps, setSteps] = useState(kit.sequence);
+	return (
+		<>
+			{kit && (
+				<section className="piano-roll fade-in">
+					<button
+						className="play-button"
+						onClick={() => setIsPlaying(!isPlaying)}>
+						{isPlaying ? " STOP " : " PLAY "}
+					</button>
 
-          <PianoRollDisplay
-            currentStepIndex={currentStepIndex}
-            steps={steps}
-            setSteps={setSteps}
-            isPlaying={isPlaying}
-          />
+					<PianoRollDisplay
+						currentStepIndex={currentStepIndex}
+						steps={steps}
+						setSteps={setSteps}
+						isPlaying={isPlaying}
+					/>
 
-          <Song isPlaying={isPlaying} bpm={kit.bpm}>
-            <Track
-              steps={steps}
-              subdivision={"16n"}
-              onStepPlay={(index) => {
-                setCurrentStepIndex(index);
-              }}
-            >
-              <Instrument
-                type="sampler"
-                samples={{
-                  C3: kit.elements.kick.sound_url,
-                  "C#3": kit.elements.snare.sound_url,
-                  D3: kit.elements.hh_closed.sound_url,
-                  "D#3": kit.elements.hh_open.sound_url,
-                  E3: kit.elements.melody.sound_url,
-                  F3: kit.elements.one_shot_1.sound_url,
-                  "F#3": kit.elements.one_shot_2.sound_url,
-                  G3: kit.elements.texture.sound_url,
-                }}
-              />
-            </Track>
-          </Song>
-        </section>
-      )}
-    </>
-  );
+					<Song isPlaying={isPlaying} bpm={bpm}>
+						<Track
+							steps={steps}
+							subdivision={"16n"}
+							onStepPlay={(index) => {
+								setCurrentStepIndex(index);
+							}}>
+							<Instrument
+								type="sampler"
+								samples={{
+									C3: kit.elements.kick.sound_url,
+									"C#3": kit.elements.snare.sound_url,
+									D3: kit.elements.hh_closed.sound_url,
+									"D#3": kit.elements.hh_open.sound_url,
+									E3: kit.elements.melody.sound_url,
+									F3: kit.elements.one_shot_1.sound_url,
+									"F#3": kit.elements.one_shot_2.sound_url,
+									G3: kit.elements.texture.sound_url,
+								}}
+							/>
+						</Track>
+					</Song>
+				</section>
+			)}
+		</>
+	);
 };
 
 export default PianoRoll;
